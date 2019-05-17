e82915f6080a9cd70695227920d78b8f1d482041
Robin Lundin <robin.lundin@softhouse.se>
2019-05-17 10:11:12 +0200

    fixed imports

--- initialize.js
+++ initialize.js
@@ -1,8 +1,8 @@
 Reveal.initialize({
   dependencies: [
-    { src: 'reveal/plugin/markdown/marked.js' },
-    { src: 'reveal/plugin/markdown/markdown.js' },
-    { src: 'reveal/plugin/notes/notes.js', async: true },
-    { src: 'reveal/plugin/highlight/highlight.js', async: true }
+    { src: '../../reveal/plugin/markdown/marked.js' },
+    { src: '../../reveal/plugin/markdown/markdown.js' },
+    { src: '../../reveal/plugin/notes/notes.js', async: true },
+    { src: '../../reveal/plugin/highlight/highlight.js', async: true }
   ]
 });
